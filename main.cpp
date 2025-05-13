b1f014a820573c018aa3facdf35a8ac9a936c55d
Anwar Kardid <anwar.kardid@eleves.enpc.fr>
2025-05-13 11:30:28 +0200

    Base definitions

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// Imagine++ project
+// Project:  Mastermind
+// Student(s): Anwar Kardid
+
+#include<iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+#include <Imagine/Graphics.h>
+
+using namespace Imagine;
+using namespace std;
+
+
+int main() {
+    cout << "Puzzler" << endl;
+    milliSleep(1000);
+    cout << "You won !!";
+
+	return 0;
+}
