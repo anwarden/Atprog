5843e1825cb233d8d06e1631fb82e80b15465f05
Anwar Kardid <anwar.kardid@eleves.enpc.fr>
2025-05-14 00:58:23 +0200

    adding main menu, and some minor subdivion improvements

--- main.cpp
+++ main.cpp
@@ -16,49 +16,88 @@ using namespace std;
 
 typedef Image<Color> Img;
 
-const int w = 500, h = 500;
+const int w = 1000, h = 1000;
 
 const int divs = 3;
 float decal = 5;
 
+Color BACKGROUND_COLOR = GREEN;
+
+int displayMainMenu() {
+    clearWindow();
+    fillRect(0, 0, w, h, BACKGROUND_COLOR);
+
+    drawString(w / 2 - 150, 100, "PUZZLER", BLACK, 36);
+    drawString(w / 2 - 200, 150, "Un jeu de puzzle avec Imagine++", BLACK, 18);
+
+    drawString(w / 2 - 100, 250, "1. Jouer", BLACK, 24);
+    drawString(w / 2 - 100, 300, "2. Classement", BLACK, 24);
+    drawString(w / 2 - 100, 350, "3. Quitter", BLACK, 24);
+
+    drawString(w / 2 - 200, 450, "Appuyez sur un numéro pour choisir une option (1-3)", BLACK, 16);
+
+        int choice = 0;
+    Event e;
+    do {
+        getEvent(0, e);
+        if (e.type == EVT_KEY_ON) {
+            if (e.key >= '1' && e.key <= '3') {
+                choice = e.key - '0';
+            }
+        }
+    } while (choice == 0);
+
+    return choice;
+}
+
 int main() {
     // Importing new images for after
+
     Img img;
     if (!load(img,srcPath("image.jpg"))) return 0;
 
     float square_dim = min(img.width(), img.height());
     img = img.getSubImage(0, 0, square_dim, square_dim);
-    openWindow(img.width()+decal*divs, img.height()+decal*divs, "Puzzler - Atprog");
-    //Subdivide the image
-    Img puzzle_items[divs][divs];
 
-    float subLen = square_dim/divs;
-
-    cout << "hello" << endl;
-    for(int i = 0; i<divs; i++)
+    //openWindow(img.width()+decal*divs, img.height()+decal*divs, "Puzzler - Atprog");
+    openWindow(w, h, "Puzzler - Atprog");
+    int option = displayMainMenu();
+    if(option == 1)
     {
-        for(int j = 0; j<divs; j++)
-            puzzle_items[i][j] = img.getSubImage(i*subLen, j*subLen, subLen, subLen);
-    }
+        clearWindow();
+        //Subdivide the image
+        Img puzzle_items[divs][divs];
 
+        float subLen = square_dim/divs;
 
-    for(int i = 0; i<divs; i++)
-    {
-        for(int j = 0; j<divs; j++)
+        cout << "hello" << endl;
+        for(int i = 0; i<divs; i++)
         {
-            display(puzzle_items[i][j], i*(subLen+intRandom(decal/2, decal)), j*(subLen+intRandom(decal/2, decal)));
+            for(int j = 0; j<divs; j++)
+                puzzle_items[i][j] = img.getSubImage(i*subLen, j*subLen, subLen, subLen);
         }
+
+
+        for(int i = 0; i<divs; i++)
+        {
+            for(int j = 0; j<divs; j++)
+            {
+                display(puzzle_items[i][j], i*(subLen+intRandom(decal/2, decal)), j*(subLen+intRandom(decal/2, decal)));
+            }
+        }
+
+        // Some dummy tests
+
+
+        //Img testExtract = img.getSubImage(0, 0, img.width()/4, img.width()/4);
+        //display(img, 10, 10);
+        //display(testExtract, 10, 10);
+
+
+
+        //End game
+        milliSleep(1000);
+        endGraphics();
     }
-
-    // Some dummy tests
-    //Img testExtract = img.getSubImage(0, 0, img.width()/4, img.width()/4);
-    //display(img, 10, 10);
-    //display(testExtract, 10, 10);
-
-
-
-    //End game
-    milliSleep(1000);
-    endGraphics();
     return 0;
 }
